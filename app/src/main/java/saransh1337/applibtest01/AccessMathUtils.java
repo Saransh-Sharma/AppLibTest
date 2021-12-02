09ed0a3630d8de523d35ff938b78a1cf57d49fe7
Saransh <saransh1337@gmail.com>
2021-12-03 02:58:34 +0530

    first commmit

---
+++ app/src/main/java/saransh1337/applibtest01/AccessMathUtils.java
@@ -0,0 +1,12 @@
+package saransh1337.applibtest01;
+
+import com.example.calculation.MathUtils;
+
+public class AccessMathUtils {
+
+    public void foo(){
+        MathUtils mu = new MathUtils();
+
+        mu.add(3,5);
+    }
+}
